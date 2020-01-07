1823582310891f9a27ad5a7b86f59526cb4ed104
danikarik <baitursynov92@gmail.com>
2020-01-07 17:18:01 +0600

    Remove styled components

--- src/components/Fields/SignatureCheck.tsx
+++ src/components/Fields/SignatureCheck.tsx
@@ -1,13 +1,4 @@
 import React from "react"
-import styled from "styled-components"
-
-const StyledVerified = styled.span`
-  color: green;
-`
-
-const StyledNotVerified = styled.span`
-  color: red;
-`
 
 interface SignatureCheckProps {
   message: string
@@ -18,11 +9,7 @@ const SignatureCheck: React.FC<SignatureCheckProps> = ({
   message,
   verified,
 }) => {
-  if (!verified) {
-    return <StyledNotVerified>{message}</StyledNotVerified>
-  }
-
-  return <StyledVerified>{message}</StyledVerified>
+  return <span style={{ color: verified ? "green" : "red" }}>{message}</span>
 }
 
 export default SignatureCheck
